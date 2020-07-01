c212292fffe3bc4ab7020f9002f30130f7174582
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-07-01 19:49:10 -0300

    Ajuste em parse de integratio

--- src/main/java/com/apimobilidade/provider/integration/PoaTransporte.java
+++ src/main/java/com/apimobilidade/provider/integration/PoaTransporte.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
 import com.apimobilidade.collection.LinhasOnibus;
+import com.apimobilidade.resources.LinhaOnibus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PoaTransporte {
@@ -13,19 +14,20 @@ public class PoaTransporte {
 	private final String URL_LINHAS = "http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%";
 	private final String URL_ITINERARIOS = "http://www.poatransporte.com.br/php/facades/process.php?a=il&p=%s";
 	private final String URL_LINHAS_ONIBUS = URL_LINHAS + "&t=o";
-	private final String URL_LINHAS_LOTACAO = URL_LINHAS + "&t=l";;
+	private final String URL_LINHAS_LOTACAO = URL_LINHAS + "&t=l";
 	
 	private RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+	private LinhaOnibus linhaOnibus;
 	
 	public LinhasOnibus getLinhasOnibus() {
 		
 		RestTemplate restTemplate = restTemplateBuilder.build();
 		
 		HttpEntity<String> response = restTemplate.exchange(
-				URL_LINHAS_ONIBUS,
-				HttpMethod.GET,
-				null,
-				String.class
+			URL_LINHAS_ONIBUS,
+			HttpMethod.GET,
+			null,
+			String.class
 		);
 	
 		String linhasOnibusString = response.getBody();
@@ -45,7 +47,7 @@ public class PoaTransporte {
 		}
 	}
 	
-	public String getItinerarios(Long idLinha) {
+	public ListaItinerarioPoaTransporte getItinerarios(Long idLinha) {
 		
 		String url = String.format(URL_ITINERARIOS, idLinha.toString());
 		
@@ -58,24 +60,41 @@ public class PoaTransporte {
 				String.class
 		);
 		
-		return response.getBody();
+		ObjectMapper objMapper = new ObjectMapper();
 	
-		/*
-		String itinerariosString = response.getBody();
+		String itinerariosString = new String("[");
+		itinerariosString += response.getBody()
+			.replaceAll(".*(\"0\":.*)", "$1")
+			.replaceAll("\"\\d+\":", "")
+			.replaceAll("}$", "");
+		itinerariosString += "]";
+		
+		ListaItinerarioPoaTransporte itinerarios;
 		
 		try {
+			String linhaString = "{";
+			linhaString += response.getBody().replace("idlinha", "idLinha").replaceAll("\\{(.*),\"0\":.*", "$1");
+			linhaString += "}";
 			
-			ObjectMapper objMapper = new ObjectMapper();
-			LinhasOnibus linhas = objMapper.readValue(
-				itinerariosString,
-				LinhasOnibus.class
+			this.linhaOnibus = objMapper.readValue(
+				linhaString,
+				LinhaOnibus.class
 			);
 			
-			return linhas;
+			itinerarios = objMapper.readValue(
+				itinerariosString,
+				ListaItinerarioPoaTransporte.class
+			);
 			
 		} catch (Exception e) {
-			return new LinhasOnibus();
+			System.out.println(e.getMessage());
+			return new ListaItinerarioPoaTransporte();
 		}
-		*/
+		
+		return itinerarios;
+	}
+
+	public LinhaOnibus getLinha() {
+		return this.linhaOnibus;
 	}
 }

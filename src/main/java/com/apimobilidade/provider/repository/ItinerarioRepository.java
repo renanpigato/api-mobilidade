5c4e945898e49ef4b14b3bcf813db7cf29484d56
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-06-30 21:39:08 -0300

    Adicionado metodos de consulta

--- src/main/java/com/apimobilidade/provider/repository/ItinerarioRepository.java
+++ src/main/java/com/apimobilidade/provider/repository/ItinerarioRepository.java
@@ -1,6 +1,7 @@
 package com.apimobilidade.provider.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -9,4 +10,8 @@ import com.apimobilidade.entity.Itinerario;
 public interface ItinerarioRepository extends PagingAndSortingRepository<Itinerario, Integer> {
 
 	List<Itinerario> findAllByLinhaId(long linha_id);
+	
+	List<Itinerario> findAllByLatitudeAndLongitude(double latitude, double longitude);
+	
+	Optional<Itinerario> findByLatitudeAndLongitudeAndLinhaId(Double latitude, Double longitude, Long linha_id);
 }

f1b978245ac8f4020a00379d0217eb78658e44f6
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-07-01 16:25:19 -0300

    Adicionado filtro e consulta por raio de de localização

--- src/main/java/com/apimobilidade/provider/LinhasOnibusProvider.java
+++ src/main/java/com/apimobilidade/provider/LinhasOnibusProvider.java
@@ -1,9 +1,14 @@
 package com.apimobilidade.provider;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
+import com.apimobilidade.classes.Localizacao;
 import com.apimobilidade.collection.LinhasOnibus;
+import com.apimobilidade.entity.Itinerario;
 import com.apimobilidade.entity.Linha;
+import com.apimobilidade.provider.repository.ItinerarioRepository;
 import com.apimobilidade.provider.repository.LinhaRepository;
 import com.apimobilidade.resources.LinhaOnibus;
 
@@ -11,23 +16,28 @@ public class LinhasOnibusProvider {
 
 	private LinhasOnibus linhasOnibus = new LinhasOnibus();
 	private String filtroNome = new String("");
+	private double raio = 0;
+	private Localizacao localizacao = new Localizacao();
 	private LinhaRepository linhaRepository;
+	private ItinerarioRepository itinerarioRepository;
 	
 	/**
 	 * @param linhaRepository
 	 */
 	public LinhasOnibusProvider(
-		LinhaRepository linhaRepository
+		LinhaRepository linhaRepository,
+		ItinerarioRepository itinerarioRepository
 	) {
 		super();
 		this.linhaRepository = linhaRepository;
+		this.itinerarioRepository = itinerarioRepository;
 	}
 
 	/**
 	 * @return the linhasOnibus
 	 */
-	public LinhasOnibus getLinhasOnibus() {
-		return this.linhasOnibus;
+	public Collection<LinhaOnibus> getLinhasOnibus() {
+		return this.linhasOnibus.values();
 	}
 
 	public boolean isEmpty() {
@@ -38,23 +48,68 @@ public class LinhasOnibusProvider {
 		this.filtroNome = nome;
 	}
 	
+	public void setFiltroRaio(double raio) {
+		this.raio = raio;
+	}
+
+	public void setFiltroLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
+	}
+	
 	public void run() {
 		
-		Iterator<Linha> linhasDatabaseIt;
+		Iterable<Linha> linhasDatabaseIt;
 		
 		if (!this.filtroNome.isEmpty()) {
-			linhasDatabaseIt = this.linhaRepository.findAllByNome(this.filtroNome).iterator();
+			linhasDatabaseIt = this.linhaRepository.findAllByNome(this.filtroNome);
+		} else if (this.raio > 0) {
+			
+			this.localizacao.calcularRaioAtuacao(this.raio * 1000);
+			
+			
+			List<Itinerario> itinerarios = this.itinerarioRepository
+				.findAllByLatitudeBetweenOrLongitudeBetween(
+					this.localizacao.getMinimaLatitude(),
+					this.localizacao.getMaximaLatitude(),
+					this.localizacao.getMinimaLongitude(),
+					this.localizacao.getMaximaLongitude()
+			);
+			
+			Iterator<Itinerario> iteratorItinerario =  itinerarios.iterator();
+			
+			while (iteratorItinerario.hasNext()) {
+				
+				Itinerario itinerario = (Itinerario) iteratorItinerario.next();
+				
+				if (!this.linhasOnibus.containsKey(itinerario.getLinha().getId())) {
+					this.linhasOnibus.put(
+						itinerario.getLinha().getId(),
+						new LinhaOnibus(
+							itinerario.getLinha().getId(),
+							itinerario.getLinha().getIdLinha(),
+							itinerario.getLinha().getCodigo(),
+							itinerario.getLinha().getNome()
+						)
+					);
+				}
+			}
+			return;
+			
 		} else {
-			linhasDatabaseIt = this.linhaRepository.findAll().iterator();
+			linhasDatabaseIt = this.linhaRepository.findAll();
 		}
 		
-		while (linhasDatabaseIt.hasNext()) {
+		Iterator<Linha> iteratorLinhas = linhasDatabaseIt.iterator();
+		
+		while (iteratorLinhas.hasNext()) {
 			
-			Linha linhaOnibus = (Linha) linhasDatabaseIt.next();
+			Linha linhaOnibus = (Linha) iteratorLinhas.next();
 			
 			if (linhaOnibus.getTipo().equals(new String("O"))) {
-				this.linhasOnibus.add(
+				this.linhasOnibus.put(
+					linhaOnibus.getId(),
 					new LinhaOnibus(
+						linhaOnibus.getId(),
 						linhaOnibus.getIdLinha(),
 						linhaOnibus.getCodigo(),
 						linhaOnibus.getNome()
@@ -63,4 +118,5 @@ public class LinhasOnibusProvider {
 			}
 		}
 	}
+
 }

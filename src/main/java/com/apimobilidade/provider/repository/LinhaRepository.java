0fed80902f9406233244c2cbdaf260c6cc726369
renan@renanpigato.inf.br <renan@renanpigato.inf.br>
2020-07-01 16:26:45 -0300

    Alterado tipo de retorno

--- src/main/java/com/apimobilidade/provider/repository/LinhaRepository.java
+++ src/main/java/com/apimobilidade/provider/repository/LinhaRepository.java
@@ -1,15 +1,14 @@
 package com.apimobilidade.provider.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.apimobilidade.entity.Linha;
 
-public interface LinhaRepository extends PagingAndSortingRepository<Linha, Integer> {
+public interface LinhaRepository extends PagingAndSortingRepository<Linha, Long> {
 	
 	Optional<Linha> findByIdLinha(Long idLinha);
 	
-	List<Linha> findAllByNome(String nome);
+	Iterable<Linha> findAllByNome(String nome);
 }
